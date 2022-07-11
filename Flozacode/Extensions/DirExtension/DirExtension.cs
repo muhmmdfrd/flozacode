a9a184e2b7c6c5693121251d0f263a43566e55b8
muhmmdfrd <mfarid2121@gmail.com>
2022-07-11 14:08:33 +0700

    add DirExtension for don't repeat yourself in check directory and file

---
+++ Flozacode/Extensions/DirExtension/DirExtension.cs
@@ -0,0 +1,29 @@
+﻿namespace Flozacode.Extensions.DirExtension
+{
+    public static class DirExtension
+    {
+        /// <summary>
+        /// Directory or folder will be created if directory with path from parameter is doesn't exist
+        /// </summary>
+        /// <param name="path"></param>
+        public static void DirExistOrCreate(this string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        /// <summary>
+        /// File will be deleted if file with path from parameter is exists
+        /// </summary>
+        /// <param name="path"></param>
+        public static void FileExistAndDelete(this string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+    }
+}
