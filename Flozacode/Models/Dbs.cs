f8844a3014b8283f2327d6f37160c29a2239261e
muhmmdfrd <mfarid2121@gmail.com>
2022-07-14 07:07:16 +0700

    Add wrapper for dbContext and override SaveChangesAsync Method

---
+++ Flozacode/Models/Dbs.cs
@@ -0,0 +1,44 @@
+﻿using Flozacode.Extensions.ExpressionExtension;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Flozacode.Models
+{
+    public class Dbs : DbContext
+    {
+        public Dbs([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+
+        protected Dbs()
+        {
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                    .Entries()
+                    .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            foreach (var entry in entries)
+            {
+                var entity = entry.Entity;
+
+                if (entity.HasProperty("CreatedDate") &&
+                    entity.HasProperty("UpdatedDate") &&
+                    entry.State == EntityState.Added)
+                {
+                    entity.GetType().GetProperty("CreatedDate")?.SetValue(entity, DateTime.UtcNow);
+                    entity.GetType().GetProperty("UpdatedDate")?.SetValue(entity, DateTime.UtcNow);
+                }
+
+                if (entry.Entity.HasProperty("UpdatedDate") && entry.State == EntityState.Modified)
+                {
+                    entity.GetType().GetProperty("UpdatedDate")?.SetValue(entity, DateTime.UtcNow);
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
