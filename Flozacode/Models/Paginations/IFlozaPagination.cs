0601c30c0f49e6a01e6dc5ccf66a359fc65e40ec
muhmmdfrd <mfarid2121@gmail.com>
2022-07-20 11:56:47 +0700

    add an interface for pagination service

---
+++ Flozacode/Models/Paginations/IFlozaPagination.cs
@@ -0,0 +1,12 @@
+﻿namespace Flozacode.Models.Paginations
+{
+    public interface IFlozaPagination<TView, TCreate, TUpdate, TFilter> where TFilter : TableFilter
+    {
+        public Task<Pagination<TView>> GetPagedAsync(TFilter filter);
+        public Task<List<TView>> GetListAsync();
+        public Task<TView> FindAsync(long id);
+        public Task<int> CreateAsync(TCreate value);
+        public Task<int> UpdateAsync(TUpdate value);
+        public Task<int> DeleteAsync(long id);
+    }
+}
