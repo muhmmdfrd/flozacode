85fef696e501859f4d9b40e92d9cead70baaa6f2
muhmmdfrd <mfarid2121@gmail.com>
2022-07-21 16:36:39 +0700

    fixing table filter infinite recursion

--- Flozacode/Models/Paginations/TableFilter.cs
+++ Flozacode/Models/Paginations/TableFilter.cs
@@ -2,42 +2,11 @@
 {
     public class TableFilter
 	{
-		private string? _keyword { get; set; }
-		private int _current { get; set; }
-		private int _size { get; set; }
-		private string? _sortName { get; set; }
-		private string? _sortDir { get; set; }
-
-		public string Keyword
-		{
-			get => _keyword ?? string.Empty;
-			set => _keyword = value;
-		}
-
-		public int Current
-		{
-			get => Current == 0 ? 1 : Current;
-			set => Current = value;
-		}
-
-		public int Size
-		{
-			get => _size == 0 ? 10 : _size;
-			set => _size = value;
-		}
-
+		public string Keyword { get; set; } = string.Empty;
+		public int Current { get; set; } = 1;
+		public int Size { get; set; } = 10;
 		public int Skip => (Current - 1) * Size;
-
-		public string SortName
-		{
-			get => _sortName ?? string.Empty;
-			set => _sortName = value;
-		}
-
-		public string SortDir
-		{
-			get => _sortDir ?? string.Empty;
-			set => _sortDir = value;
-		}
+		public string SortName { get; set; } = string.Empty;
+		public string SortDir { get; set; } = string.Empty;
 	}
 }
