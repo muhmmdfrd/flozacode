c434bf345ae8eae688edc5ef9f42c356f1f00e71
muhmmdfrd <mfarid2121@gmail.com>
2022-09-14 09:42:13 +0700

    add int extension test

---
+++ Flozacode.UnitTest/Extensions/IntExtensionTest.cs
@@ -0,0 +1,22 @@
+﻿using Flozacode.Extensions.NumberExtension;
+using Flozacode.Helpers.StringHelper;
+
+namespace Flozacode.UnitTest.Extensions;
+
+[TestClass]
+public class IntExtensionTest
+{
+    [TestMethod]
+    public void ConvertToHashCodeTest()
+    {
+        var number = FlozaString.GenerateRandomNumberString().ToInt();
+        var key = FlozaString.GenerateRandomString();
+        var hashCode = number.ToHashCode(key);
+        
+        Console.WriteLine($"Number: {number}");
+        Console.WriteLine($"Key: {key}");
+        Console.WriteLine($"hashcode: {hashCode}");
+        
+        Assert.IsTrue(true);
+    }
+}
